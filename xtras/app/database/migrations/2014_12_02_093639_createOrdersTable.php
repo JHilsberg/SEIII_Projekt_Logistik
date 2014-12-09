7b4ef8fc3aec8761e0601e45fe9736d790da2a1c
Nippel69 <nephro13@googlemail.com>
2014-12-09 15:30:16 +0100

    database

--- xtras/app/database/migrations/2014_12_02_093639_createOrdersTable.php
+++ xtras/app/database/migrations/2014_12_02_093639_createOrdersTable.php
@@ -12,18 +12,55 @@ class CreateOrdersTable extends Migration {
 	 */
 	public function up()
 	{
-        Schema::create('orders', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('userid')->references('id')->on('users');
-            $table->integer('abholadresse')->references('id')->on('addresses');
 
-            $table->integer('lieferadresse')->references('id')->on('addresses');
+            $table->string('vorname', 32);
+            $table->string('nachname', 32);
+            $table->string('firma', 32);
+
+            $table->string('strasse', 64);
+            $table->string('hausnummer', 5);
+            $table->string('ort', 64);
+            $table->string('postleitzahl', 10);
+            $table->string('land', 32);
+
+
+            $table->string('username', 32);
+            $table->string('email', 320);
+            $table->string('password', 64);
+
+            // required for Laravel 4.1.26
+            $table->string('remember_token', 100)->nullable();
+
+            $table->timestamps();
+        });
+
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('firma', 32);
+            $table->string('strasse', 64);
+            $table->string('hausnummer', 5);
+            $table->string('ort', 64);
+            $table->string('postleitzahl', 10);
+        });
+
+        Schema::create('orders', function (Blueprint $table) {
+
+            //$table->engine= 'InnoDB';
+            $table->increments('id');
+            $table->integer('userid')->unsigned();
+            $table->foreign('userid')->references('id')->on('users');
+            $table->integer('abholadresse')->unsigned();
+            $table->foreign('abholadresse')->references('id')->on('addresses');
+            $table->integer('lieferadresse')->unsigned();
+            $table->foreign('lieferadresse')->references('id')->on('addresses');
 
             $table->date('lieferdatum',10);
             $table->date('minlieferzeit',10);
             $table->date('maxlieferzeit',10);
 
-            $table->integer('anzahltransportbehaelter',50);
+            $table->integer('anzahltransportbehaelter');
             $table->string('transportbehaelter',50);
             $table->string('warenbeschreibung',250);
             $table->boolean('gefahrengut');
@@ -36,8 +73,7 @@ class CreateOrdersTable extends Migration {
             $table->boolean('pkw');
             $table->boolean('flugzeug');
             $table->boolean('egal');
-
-            $table->timestamp('abegesendet');
+            $table->timestamp('abgesendet');
 
 
         });
@@ -51,6 +87,8 @@ class CreateOrdersTable extends Migration {
 	public function down()
 	{
 		Schema::drop('orders');
+        Schema::drop('users');
+        Schema::drop('addresses');
 	}
 
 }
