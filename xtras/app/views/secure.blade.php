efa4162666c35bfc11d88867eeb3a77c5de7710a
Julian_Thinkpad <Julian>
2014-12-13 14:33:50 +0100

    use Bootstrap-CSS for Formular Design, added formular.js and formular.css

--- xtras/app/views/secure.blade.php
+++ xtras/app/views/secure.blade.php
@@ -6,40 +6,14 @@
     {{ HTML::style('css/bootstrap.css') }}
     {{ HTML::style('css/bootstrap-theme.css') }}
     {{ HTML::style('css/formular.css') }}
-    {{ HTML::style('css/datepicker.css') }}
+
     {{ HTML::style('css/datepicker3.css') }}
     <!-- Scripts are placed here -->
     {{ HTML::script('js/jquery-2.1.1.js') }}
     {{ HTML::script('js/bootstrap.min.js') }}
     {{ HTML::script('js/bootbox.min.js') }}
-    {{ HTML::script('js/login.js') }}
+    {{ HTML::script('js/formular.js') }}
     {{ HTML::script('js/bootstrap-datepicker.js') }}
-
-
-
-
-<script type="text/javascript">
-$(document).ready(function() {
-    $('#calendar').datepicker({
-    });
-    $('#calendar').keydown(function(event){
-    return false;
-    });
-} );
-</script>
-<script type="text/javascript">
-$(document).ready(function() {
-    $('#calendar2').datepicker({
-    });
-} );
-</script>
-<script type="text/javascript">
-$(document).ready(function() {
-    $('#calendar3').datepicker({
-    });
-} );
-</script>
-
 </head>
 <body>
 <nav role="navigation" class="navbar navbar-default">
@@ -108,15 +82,15 @@ $(document).ready(function() {
 <div class="form-group">
 <div class="col-md-4">
 <h2>{{Lang::get('formular.loading_place')}}</h2>
-<p class="form-paragraph">{{ Form::text('lp_name', '',array('placeholder' => Lang::get('formular.name'), 'class' => 'form-control datepicker, form-datepicker'));}}</p>
+<p class="form-paragraph">{{ Form::text('lp_name', '',array('placeholder' => Lang::get('formular.name'), 'class' => 'form-control'));}}</p>
 
-     <div style="height: 30dpi">
+
           @if($errors->first('lp_name'))
               <div class="alert alert-info">
                   {{ $errors->first('lp_name') }}
                </div>
            @endif
-      </div>
+
 
 
 
@@ -125,99 +99,85 @@ $(document).ready(function() {
         {{ Form::text('lp_number', '',array('placeholder' => Lang::get('formular.number'), 'class' => 'form-control'));}}
 </p>
 
-
-     <div style="height: 30dpi">
           @if($errors->first('lp_street'))
               <div class="alert alert-info">
                   {{ $errors->first('lp_street') }}
                </div>
            @endif
-      </div>
 
-
-     <div style="height: 30dpi">
           @if($errors->first('lp_number'))
               <div class="alert alert-info">
                   {{ $errors->first('lp_number') }}
                </div>
            @endif
-      </div>
 
 
-<p class="form-paragraph">{{ Form::text('lp_plz', '',array('placeholder' => Lang::get('formular.plz')));}}
-{{ Form::text('lp_city', '',array('placeholder' => Lang::get('formular.city')));}}</p>
+
+<p class="form-paragraph form-inline">
+    {{ Form::text('lp_plz', '',array('placeholder' => Lang::get('formular.plz'), 'class' => 'form-control'));}}
+    {{ Form::text('lp_city', '',array('placeholder' => Lang::get('formular.city'), 'class' => 'form-control'));}}
+</p>
 
 
-     <div style="height: 30dpi">
-          @if($errors->first('lp_plz'))
+           @if($errors->first('lp_plz'))
               <div class="alert alert-info">
                   {{ $errors->first('lp_plz') }}
                </div>
            @endif
-      </div>
 
 
-     <div style="height: 30dpi">
           @if($errors->first('lp_city'))
               <div class="alert alert-info">
                   {{ $errors->first('lp_city') }}
                </div>
            @endif
-      </div>
-
 
 </div>
 <div class="col-md-4">
 <h2>{{Lang::get('formular.deliveryPlace')}}</h2>
-<p class="form-paragraph">{{ Form::text('dp_name', '',array('placeholder' => Lang::get('formular.name')));}}</p>
+<p class="form-paragraph">
+    {{ Form::text('dp_name', '',array('placeholder' => Lang::get('formular.name'), 'class' => 'form-control'));}}
+</p>
 
-     <div style="height: 30dpi">
           @if($errors->first('dp_name'))
               <div class="alert alert-info">
                   {{ $errors->first('dp_name') }}
                </div>
            @endif
-      </div>
 
-<p class="form-paragraph">{{ Form::text('dp_street', '',array('placeholder' => Lang::get('formular.street')));}}
-{{ Form::text('dp_number', '',array('placeholder' => Lang::get('formular.number')));}}</p>
+<p class="form-paragraph form-inline">
+{{ Form::text('dp_street', '',array('placeholder' => Lang::get('formular.street'), 'class' => 'form-control'));}}
+{{ Form::text('dp_number', '',array('placeholder' => Lang::get('formular.number'), 'class' => 'form-control'));}}</p>
 
-     <div style="height: 30dpi">
-          @if($errors->first('dp_street'))
-              <div class="alert alert-info">
-                  {{ $errors->first('dp_street') }}
-               </div>
-           @endif
-      </div>
+    @if($errors->first('dp_street'))
+        <div class="alert alert-info">
+            {{ $errors->first('dp_street') }}
+        </div>
+    @endif
 
-           <div style="height: 30dpi">
-                @if($errors->first('dp_number'))
-                    <div class="alert alert-info">
-                        {{ $errors->first('dp_number') }}
-                     </div>
-                 @endif
-            </div>
+    @if($errors->first('dp_number'))
+        <div class="alert alert-info">
+            {{ $errors->first('dp_number') }}
+        </div>
+    @endif
 
 
-<p class="form-paragraph">{{ Form::text('dp_plz', '',array('placeholder' => Lang::get('formular.plz')));}}
-{{ Form::text('dp_city', '',array('placeholder' => Lang::get('formular.city')));}}</p>
+<p class="form-paragraph form-inline">
+    {{ Form::text('dp_plz', '',array('placeholder' => Lang::get('formular.plz'), 'class' => 'form-control'));}}
+    {{ Form::text('dp_city', '',array('placeholder' => Lang::get('formular.city'), 'class' => 'form-control'));}}
+</p>
 
-     <div style="height: 30dpi">
           @if($errors->first('dp_plz'))
               <div class="alert alert-info">
                   {{ $errors->first('dp_plz') }}
                </div>
            @endif
-      </div>
 
-           <div style="height: 30dpi">
                 @if($errors->first('dp_city'))
                     <div class="alert alert-info">
                         {{ $errors->first('dp_city') }}
                      </div>
                  @endif
-            </div>
-
 
 </div>
 </div>
@@ -225,26 +185,26 @@ $(document).ready(function() {
 <div class="form-group">
 <div class="col-md-4">
 <h2 class="">{{Lang::get('formular.pickup_delivery')}}</h2>
-<p class="form-paragraph">{{ Form::text('abholtermin', null, array('type' => 'text', 'class' => 'form-control datepicker, form-datepicker','placeholder' => Lang::get('formular.pickup'), 'id' => 'calendar')) }}
+<p class="form-paragraph">{{ Form::text('abholtermin', null, array('type' => 'text', 'class' => 'form-control','placeholder' => Lang::get('formular.pickup'), 'id' => 'calendar')) }}
 </p>
-<p class="form-paragraph">{{ Form::text('minLiefertermin', null, array('type' => 'text', 'class' => 'form-control datepicker, form-datepicker','placeholder' => Lang::get('formular.min_delivery'), 'id' => 'calendar2')) }}
+<p class="form-paragraph">{{ Form::text('minLiefertermin', null, array('type' => 'text', 'class' => 'form-control','placeholder' => Lang::get('formular.min_delivery'), 'id' => 'calendar2')) }}
 </p>
-<p class="form-paragraph">{{ Form::text('maxLiefertermin', null, array('type' => 'text', 'class' => 'form-control datepicker, form-datepicker','placeholder' => Lang::get('formular.max_delivery'), 'id' => 'calendar3')) }}
+<p class="form-paragraph">{{ Form::text('maxLiefertermin', null, array('type' => 'text', 'class' => 'form-control','placeholder' => Lang::get('formular.max_delivery'), 'id' => 'calendar3')) }}
 </p>
 
 
 <div>
 <h2>{{Lang::get('formular.casket')}}</h2>
-<p class="form-paragraph">{{Form::number('anzahlBehaelter', 'value',array('placeholder' =>Lang::get('formular.quantity')));}}
-{{Form::select('behaelter', array('C' => Lang::get('formular.container'), 'P' => Lang::get('formular.pallet'), 'B' => Lang::get('formular.boxes')), 'C');}}
+<p class="form-paragraph form-inline">
+    {{Form::number('anzahlBehaelter', 'value',array('placeholder' =>Lang::get('formular.quantity'), 'class' => 'form-control'));}}
+    {{Form::select('behaelter', array('C' => Lang::get('formular.container'), 'P' => Lang::get('formular.pallet'), 'B' => Lang::get('formular.boxes')), 'C',array('class' => 'form-control'));}}
 </p>
-     <div style="height: 30dpi">
+
           @if($errors->first('anzahlBehaelter'))
               <div class="alert alert-info">
                   {{ $errors->first('anzahlBehaelter') }}
                </div>
            @endif
-      </div>
 
 </div>
 </div>
@@ -266,50 +226,45 @@ $(document).ready(function() {
 <div class="col-md-4">
 <h2>{{Lang::get('formular.prod_info')}}</h2>
 <p class="form-paragraph">{{ Form::checkbox('gefahrgut', '1');}} {{Lang::get('formular.dangerous')}}</p>
-<p class="form-paragraph">{{Form::textarea('beschreibung', '',array('type' => 'text', 'class' => 'form-textarea','placeholder' => Lang::get('formular.description_goods')));}}</p>
+<p class="form-paragraph">{{Form::textarea('beschreibung', '',array('type' => 'text', 'class' => 'form-control','placeholder' => Lang::get('formular.description_goods')));}}</p>
 
-     <div style="height: 30dpi">
           @if($errors->first('beschreibung'))
               <div class="alert alert-info">
                   {{ $errors->first('beschreibung') }}
                </div>
            @endif
-      </div>
 
-<p class="form-paragraph">{{Form::number('gewicht', 'value',array('placeholder'=>Lang::get('formular.weight')));}}
-{{Form::select('einheit', array('KG' => Lang::get('formular.kilogram'), 'T' => Lang::get('formular.ton')), 'T');}}</p>
+<p class="form-paragraph form-inline">
+{{Form::number('gewicht', 'value',array('placeholder'=>Lang::get('formular.weight'), 'class' => 'form-control'));}}
+{{Form::select('einheit', array('KG' => Lang::get('formular.kilogram'), 'T' => Lang::get('formular.ton')), 'T', array('class' => 'form-control'));}}</p>
 
-     <div style="height: 30dpi">
           @if($errors->first('gewicht'))
               <div class="alert alert-info">
                   {{ $errors->first('gewicht') }}
                </div>
            @endif
-      </div>
 
-<p class="form-paragraph">{{ Form::text('verpackung', '',array('placeholder' => Lang::get('formular.packaging'), 'class' => 'form-control datepicker, form-datepicker'));}}</p>
+<p class="form-paragraph">
+{{ Form::text('verpackung', '',array('placeholder' => Lang::get('formular.packaging'), 'class' => 'form-control'));}}</p>
 
-     <div style="height: 30dpi">
-          @if($errors->first('verpackung'))
-              <div class="alert alert-info">
+        @if($errors->first('verpackung'))
+            <div class="alert alert-info">
                   {{ $errors->first('verpackung') }}
-               </div>
-           @endif
-      </div>
+            </div>
+        @endif
 </div>
 
 <div class="col-md-4">
 <h2>{{Lang::get('formular.other')}}</h2>
 <div class="top">
-<p class="form-paragraph">{{Form::textarea('bemerkung', '',array('type' => 'text', 'class' => 'form-textarea','placeholder' => Lang::get('formular.comment')))}}</p>
+<p class="form-paragraph">
+{{Form::textarea('bemerkung', '',array('type' => 'text', 'class' => 'form-control','placeholder' => Lang::get('formular.comment')))}}</p>
 
-     <div style="height: 30dpi">
-          @if($errors->first('bemerkung'))
-              <div class="alert alert-info">
-                  {{ $errors->first('bemerkung') }}
-               </div>
-           @endif
-      </div>
+        @if($errors->first('bemerkung'))
+            <div class="alert alert-info">
+                {{ $errors->first('bemerkung') }}
+            </div>
+        @endif
 
 </div>
 </div>
---
+++ xtras/public/js/formular.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Julian on 09.12.2014.
+ */
+$(document).ready(function() {
+    $('#calendar').datepicker({
+    });
+    $('#calendar2').datepicker({
+    });
+    $('#calendar3').datepicker({
+    });
+} );
