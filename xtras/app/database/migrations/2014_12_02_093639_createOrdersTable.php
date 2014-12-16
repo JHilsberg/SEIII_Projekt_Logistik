5887e511233fa78eff73b9c09cc198b39a769e3f
Nippel69 <nephro13@googlemail.com>
2014-12-16 12:38:31 +0100

    default werte Datenbank

--- xtras/app/database/migrations/2014_12_02_093639_createOrdersTable.php
+++ xtras/app/database/migrations/2014_12_02_093639_createOrdersTable.php
@@ -38,11 +38,11 @@ class CreateOrdersTable extends Migration {
 
         Schema::create('addresses', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('firma', 32);
-            $table->string('strasse', 64);
-            $table->string('hausnummer', 5);
-            $table->string('ort', 64);
-            $table->string('postleitzahl', 10);
+            $table->string('firma', 32)->default(0);
+            $table->string('strasse', 64)->default(0);
+            $table->string('hausnummer', 5)->default(0);
+            $table->string('ort', 64)->default(0);
+            $table->string('postleitzahl', 10)->default(0);
         });
 
         Schema::create('orders', function (Blueprint $table) {
@@ -60,13 +60,13 @@ class CreateOrdersTable extends Migration {
             $table->date('minlieferzeit',10);
             $table->date('maxlieferzeit',10);
 
-            $table->integer('anzahltransportbehaelter');
-            $table->string('transportbehaelter',50);
-            $table->string('warenbeschreibung',250);
+            $table->integer('anzahltransportbehaelter')->default(0);
+            $table->string('transportbehaelter',50)->default(0);
+            $table->string('warenbeschreibung',250)->default(0);
             $table->boolean('gefahrengut')->default(0);
-            $table->string('warenverpackung',250);
-            $table->float('warengewicht',20);
-            $table->string('bemerkung',250);
+            $table->string('warenverpackung',250)->default(0);
+            $table->float('warengewicht',20)->default(0);
+            $table->string('bemerkung',250)->default(0);
             $table->boolean('lkw')->default(0);
             $table->boolean('schiff')->default(0);
             $table->boolean('zug')->default(0);
