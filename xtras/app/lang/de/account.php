06e6e1fefc7328fa832d2666ed67ac6c21994c8a
Julian_Thinkpad <Julian>
2015-01-06 11:20:08 +0100

    added account language files and css

---
+++ xtras/app/lang/de/account.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: besitzer
+ * Date: 16.12.14
+ * Time: 10:53
+ */
+
+return array(
+    'edit'               => 'Ändern',
+    'surname'            => 'Vorname',
+    'name'               => 'Nachname',
+    'Strasse'            => 'Straße',
+    'hausnummer'         => 'Hausnummer',
+    'plz'                => 'Postleitzahl',
+    'ort'                => 'Ort',
+    'email'              => 'E-Mail',
+    'save'               => 'Speichern',
+    'land'               => 'Land'
+);
---
+++ xtras/app/lang/en/account.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: besitzer
+ * Date: 16.12.14
+ * Time: 10:53
+ */
+
+return array(
+    'edit'               => 'Edit',
+    'surname'            => 'Surname',
+    'name'               => 'Name',
+    'firma'              => 'Company-Name',
+    'strasse'            => 'Street',
+    'hausnummer'         => 'House number',
+    'plz'                => 'Pincode',
+    'ort'                => 'City',
+    'email'              => 'E-Mail',
+    'save'               => 'Save',
+     'land'              => 'Country'
+);
